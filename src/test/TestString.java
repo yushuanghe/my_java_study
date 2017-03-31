eb399726b0799fe476d6fbc5bdbd5bdea05533b2
yushuanghe <yushuanghe92@outlook.com>
2017-03-31 16:16:52 +0800

    测试String

--- src/test/TestString.java
+++ src/test/TestString.java
@@ -25,5 +25,34 @@ public class TestString {
         String x2 = key1 + key2;
         System.out.println(x1 == x2);
         System.out.println(x1.equals(x2));
+
+
+        System.out.println("-----我是分割线-----");
+
+
+        /**
+         * StringBuffer没有实现equals和hashcode
+         */
+        System.out.println(new String("abc").equals(new String("abc")));
+        System.out.println(new StringBuffer("abc").equals(new StringBuffer("abc")));
+        System.out.println("-----我是分割线-----");
+
+        String a = "abcd";
+        String b = "ab" + "cd";
+        System.out.println(a == b);//true
+        String t = "cd";
+        String c = "ab" + t;
+        System.out.println(a == c);//false
+        final String s = "cd";
+        String d = "ab" + s;
+        System.out.println(a == d);//true
+        String r = "ab";
+        String e = r + t;
+        System.out.println(a == e);//false
+        System.out.println(e.intern() == a);//true
+        String f = new String("abcd");
+        System.out.println(a == f);//false
+        System.out.println(f.intern() == a);//true
+        System.out.println(f.intern() == a.intern());//true
     }
 }
